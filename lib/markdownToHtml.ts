38bc576e4609f9792af0966281eb24eadf750389
Joo <joo@joostory.net>
2023-01-25 23:36:44 +0900

    fix error

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,7 +2,7 @@ import { remark } from 'remark'
 import remarkHtml from 'remark-html'
 import remarkGfm from 'remark-gfm'
 
-export default async function markdownToHtml(markdown) {
+export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(remarkGfm)
     .use(remarkHtml)
