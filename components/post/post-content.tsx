29122a1e65c35998633a801cba51742a18b4cb31
Joo <joo@joostory.net>
2023-03-17 23:52:04 +0900

    react-syntax-highlighter style 오류 수정

--- components/post/post-content.tsx
+++ components/post/post-content.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function PostContent({content}: {content: string}) {
@@ -9,7 +9,7 @@ export default function PostContent({content}: {content: string}) {
       className="prose"
       remarkPlugins={[remarkGfm]}
       components={{
-        code({node, inline, className, children, ...props}) {
+        code({node, inline, className, children, style, ...props}) {
           const match = /language-(\w+)/.exec(className || '')
           return !inline && match ? (
             <SyntaxHighlighter
