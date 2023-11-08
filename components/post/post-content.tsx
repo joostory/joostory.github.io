78048fa1e42e5d50101a180d5a927f6f7d71fb05
Joo <joo@joostory.net>
2023-11-09 00:05:51 +0900

    nextjs build 오류 수정

--- components/post/post-content.tsx
+++ components/post/post-content.tsx
@@ -9,14 +9,13 @@ export default function PostContent({content}: {content: string}) {
       className="prose"
       remarkPlugins={[remarkGfm]}
       components={{
-        code({node, inline, className, children, style, ...props}) {
+        code({node, className, children, style, ...props}) {
           const match = /language-(\w+)/.exec(className || '')
-          return !inline && match ? (
+          return match ? (
             <SyntaxHighlighter
               style={materialDark}
               language={match[1]}
               PreTag="div"
-              {...props}
             >
               {String(children).replace(/\n$/, '')}
             </SyntaxHighlighter>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true
