a7a9a376be3c761e73e97e7b30d04282014297c2
Joo <joo@joostory.net>
2025-03-02 11:41:47 +0900

    lib update (daisyui 5.0)

--- components/post/post-content.tsx
+++ components/post/post-content.tsx
@@ -5,29 +5,30 @@ import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export default function PostContent({content}: {content: string}) {
   return (
-    <ReactMarkdown
-      className="prose"
-      remarkPlugins={[remarkGfm]}
-      components={{
-        code({node, className, children, style, ...props}) {
-          const match = /language-(\w+)/.exec(className || '')
-          return match ? (
-            <SyntaxHighlighter
-              style={materialDark}
-              language={match[1]}
-              PreTag="div"
-            >
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          )
-        }
-      }}
-    >
-      {content}
-    </ReactMarkdown>
+    <div className="prose">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({node, className, children, style, ...props}) {
+            const match = /language-(\w+)/.exec(className || '')
+            return match ? (
+              <SyntaxHighlighter
+                style={materialDark}
+                language={match[1]}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   )
 }
