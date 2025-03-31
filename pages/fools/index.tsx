491933368b57606c3c22cd795ef7bfdd502b9a5c
Joo <joo@joostory.net>
2025-03-31 23:32:56 +0900

    FoolPage 컴포넌트의 메타데이터 및 설명 수정, 이미지 업데이트

--- pages/fools/index.tsx
+++ pages/fools/index.tsx
@@ -7,17 +7,14 @@ const font = Single_Day({
 
 export default function FoolPage() {
   const title = "오픈채팅, 글로벌 열풍 속 사상 최대 실적 달성!"
-  const description = `오늘 오픈채팅이 자사의 공식 블로그를 통해 2024년 연간 실적 보고서를 발표하며 놀라운 성과를 공개했습니다. 회사는 국내외 사용자 급증과 함께 사상 최대 매출을 기록했다고 전했습니다. 무엇보다도 올해 초, 오픈채팅은 글로벌 서비스 확장에 성공하며 현재 전 세계 50개국 이상에서 활발히 사용되고 있습니다.
-회사는 이 과정에서 'AI 비서 통합 기능'을 도입해 사용자들이 실시간 번역, 자동 응답, 비서형 채팅 도우미 등을 활용할 수 있도록 지원했습니다. 이 기능은 해외 사용자들로부터 폭발적인 반응을 얻었으며, 특히 라틴 아메리카와 동유럽에서 급격한 성장을 기록하며 새로운 시장을 개척했다고 합니다.
-오픈채팅 대표는 "이 모든 성과는 사용자들의 신뢰와 사랑 덕분입니다. 앞으로도 더욱 새롭고 혁신적인 서비스를 제공해 전 세계인의 마음을 사로잡겠습니다"라고 밝혔습니다.
-한편, 업계에서는 "오픈채팅이 다음 분기 중 IPO를 준비 중"이라는 소문까지 돌며 큰 주목을 받고 있습니다.`
+  const description = `오픈채팅, 글로벌 열풍 속 사상 최대 실적 달성! 50개국 이상에서 성공적으로 사용되며, AI 통합 기능으로 폭발적 성장. IPO 소문까지 화제!`
   const imageUrl = "/assets/fools/fools_og.png"
 
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
+        <title>{'만우절 농담'}</title>
+        <meta name="description" content={'농담이지롱!'} />
         
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
@@ -33,7 +30,7 @@ export default function FoolPage() {
         <img src="/assets/fools/fools.png" alt="Fool" className="rounded-xl" />
 
         <p className="text-4xl font-bold text-center mt-10 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text">
-          <span className={font.className}>좋은 하루 되세요!</span>
+          <span className={font.className}>오늘도 좋은 하루 되세요!</span>
         </p>
       </div>
     </>
